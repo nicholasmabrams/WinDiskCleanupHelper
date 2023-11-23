abab2eb062d92b1fdf667438110e6e094fce3b58
Abrams, Nicholas <abramsn@nicholasabrams.com>
2023-11-23 02:06:25 -0500

    Initial commit for Windows disk cleanup helper.

---
+++ WinDiskCleanupHelper.ps1
@@ -0,0 +1,62 @@
+﻿#Requires -Version 7
+Param($topNFiles = 10, $target="C:/")
+
+Function Get-Largest-Files {
+    param($NLargestFiles, $target) 
+
+    # Get all the files from all the drives and sort them by size in descending order
+    $biggestFiles = Get-ChildItem -Path $target -Recurse -File -ErrorAction SilentlyContinue | Sort-Object Length -Descending | Select-Object -First $topNFiles | ForEach-Object -Parallel {
+            $size = $_.Length;    
+            $curFile = "$($_.FullName) | $($_.LastAccessTime) | "; 
+            
+            If ($size -gt 1GB) {
+                $curFile += "{0:N2} GB" -f ($size / 1GB)
+            }
+            ElseIf ($size -gt 1MB) {
+                $curFile +="{0:N2} MB" -f ($size / 1MB)
+            }
+            ElseIf ($size -gt 1KB) {
+                $curFile += "{0:N2} KB" -f ($size / 1KB)
+            }
+            Else {
+                $curFile += "{0:N2} BYTES" -f $size
+            }
+
+            Write-Host $curFile`r`n
+            Write-Output $curFile`r`n
+    }
+}
+
+Function Get-Most-Recent {
+    param($NLargestFiles, $target) 
+
+    # Get all the files from all the drives and sort them by size in descending order
+    $biggestFiles = Get-ChildItem -Path $target -Recurse -File -ErrorAction SilentlyContinue | Sort-Object Length -Descending | Sort-Object LastAccessTime -Descending | Select-Object -First $topNFiles | ForEach-Object -Parallel {
+            $size = $_.Length;    
+            $curFile = "$($_.FullName) | $($_.LastAccessTime) | "; 
+            
+            If ($size -gt 1GB) {
+                $curFile += "{0:N2} GB" -f ($size / 1GB)
+            }
+            ElseIf ($size -gt 1MB) {
+                $curFile +="{0:N2} MB" -f ($size / 1MB)
+            }
+            ElseIf ($size -gt 1KB) {
+                $curFile += "{0:N2} KB" -f ($size / 1KB)
+            }
+            Else {
+                $curFile += "{0:N2} BYTES" -f $size
+            }
+
+            Write-Host $curFile`r`n`n
+            Write-Output $curFile`r`n`n
+    }
+}
+
+Write-Host "Searching for the largest $($topNFiles) files starting from $($target)"`r`n
+Echo 'Getting largest files...'`n 
+Get-Largest-Files `r`n
+Echo "`r`n The most recent  $($topNFiles) files accessed, while searching recursively starting from $($target)"`r`n
+Get-Most-Recent `r`n
+Echo "The most recent $($topNFiles) files touched on disk, recursively starting from $($target)"`r`n
+Write-Debug $psversiontable 
